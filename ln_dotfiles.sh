47467ea35396937307bf18b9ebf8aa1be0ae591f
HIRAKI Satoru <saruko313@gmail.com>
2013-06-28 02:34:52 +0900

    make ln script

---
+++ ln_dotfiles.sh
@@ -0,0 +1,14 @@
+!#/bin/sh
+
+cd $HOME
+ln -s $HOME/dotfiles/.ctags $HOME/.ctags
+ln -s $HOME/dotfiles/.emacs.d $HOME/.emacs.d
+ln -s $HOME/dotfiles/.gvimrc $HOME/.gvimrc
+ln -s $HOME/dotfiles/.tmux-powerlinerc $HOME/.tmux-powerlinerc
+ln -s $HOME/dotfiles/.tmux.conf $HOME/.tmux.conf
+ln -s $HOME/dotfiles/.vim $HOME/.vim
+ln -s $HOME/dotfiles/.vimperator $HOME/.vimperator
+ln -s $HOME/dotfiles/.vimperatorrc $HOME/.vimperatorrc
+ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
+ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+

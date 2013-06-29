aa6afa733aaf21cd7d0b39549eda3d09a98b735e
HIRAKI Satoru <saruko313@gmail.com>
2013-06-30 03:31:12 +0900

    add .zsh

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -11,4 +11,5 @@ ln -s $HOME/dotfiles/.vimperator $HOME/.vimperator
 ln -s $HOME/dotfiles/.vimperatorrc $HOME/.vimperatorrc
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
+ln -s $HOME/dotfiles/.zsh $HOME/.zsh
 

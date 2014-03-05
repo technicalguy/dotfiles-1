992f172ff59d5ac4ae11ada2157c08f02c0f73f4
HIRAKI Satoru <saruko313@gmail.com>
2014-03-05 14:01:02 +0900

    change emacs setting files
    use evil mode

---
+++ .emacs.d/inits/20-evil.el
@@ -0,0 +1,8 @@
+;; evil
+(require 'evil)
+(evil-mode t)
+
+;; evil-numbers
+(require 'evil-numbers)
+(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)

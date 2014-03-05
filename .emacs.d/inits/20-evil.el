a20157d3e3e7eb51e9d93421d6061036b70db76c
HIRAKI Satoru <saruko313@gmail.com>
2014-03-05 17:58:43 +0900

    change keybind of evil-numbers

--- .emacs.d/inits/20-evil.el
+++ .emacs.d/inits/20-evil.el
@@ -2,10 +2,10 @@
 (require 'evil)
 (evil-mode t)
 
-;; evil-numbers
+ evil-numbers
 (require 'evil-numbers)
-(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+(define-key evil-normal-state-map (kbd "C-c +") 'evil-numbers/inc-at-pt)
+(define-key evil-normal-state-map (kbd "C-c -") 'evil-numbers/dec-at-pt)
 
-;; evil-tabs
-(global-evil-tabs-mode t)
+;; evimmmmmm
+mmlobal-evil-tabs-mode t)

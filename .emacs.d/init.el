a0dc697e56d2d09d263ac6cea3e130f5b6265271
HIRAKI Satoru <saruko313@gmail.com>
2015-12-24 15:06:02 +0900

    use spaceemacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,30 +1,32 @@
-;; Emacs directory
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+;;; init.el --- Spacemacs Initialization File
+;;
+;; Copyright (c) 2012-2014 Sylvain Benner
+;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
 
-;; load-path
-(setq load-path
-      (append
-       (list
-        (expand-file-name "~/.emacs.d/")
-        (expand-file-name "~/.emacs.d/elisp/")
-        (expand-file-name "~/.emacs.d/elisp/el-get/el-get/"))
-       load-path))
+;; Without this comment emacs25 adds (package-initialize) here
+;; (package-initialize)
 
-;; package.el
-(require 'package)
-(add-to-list 'package-archives
-              '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+(setq gc-cons-threshold 100000000)
+(defconst spacemacs-version          "0.104.8" "Spacemacs version.")
+(defconst spacemacs-emacs-min-version   "24.3" "Minimal version of Emacs.")
 
-(require 'init-loader)
-(setq init-loader-show-log-after-init nil)
-(init-loader-load (expand-file-name "~/.emacs.d/inits"))
+(defun spacemacs/emacs-version-ok ()
+  (version<= spacemacs-emacs-min-version emacs-version))
 
-;; color
-(load-theme 'solarized-dark t)
-
-;; color-moccur
-(require 'color-moccur)
+(when (spacemacs/emacs-version-ok)
+  (load-file (concat user-emacs-directory "core/core-load-paths.el"))
+  (require 'core-spacemacs)
+  (require 'core-configuration-layer)
+  (spacemacs/init)
+  (spacemacs/maybe-install-dotfile)
+  (configuration-layer/sync)
+  (spacemacs/setup-startup-hook)
+  (require 'server)
+  (unless (server-running-p) (server-start)))
